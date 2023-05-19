8581d1cb9347b0b5821e7141d544283b659686e8
vitekkor <viktor.korotkikh@gmail.com>
2023-05-20 00:18:42 +0300

    #5: provide configuration; add MainScenario

---
+++ compoly-bot/src/main/kotlin/com/vitekkor/compolybot/config/properties/BotConfigurationProperties.kt
@@ -0,0 +1,8 @@
+package com.vitekkor.compolybot.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "bot")
+data class BotConfigurationProperties(val telegramToken: String)
---
+++ compoly-bot/src/main/kotlin/com/vitekkor/compolybot/scenario/MainScenario.kt
@@ -0,0 +1,24 @@
+package com.vitekkor.compolybot.scenario
+
+import com.justai.jaicf.builder.Scenario
+import com.justai.jaicf.channel.telegram.telegram
+import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.model.scenario.ScenarioModel
+import com.justai.jaicf.model.scenario.getValue
+import org.springframework.stereotype.Component
+
+@Component
+class MainScenario : Scenario {
+    override val model: ScenarioModel by Scenario {
+        state("start") {
+            activators { regex("/start") }
+            action(telegram) {
+                reactions.say("Привет!")
+            }
+        }
+
+        fallback {
+            reactions.say("Sorry, I didn't get it...")
+        }
+    }
+}
