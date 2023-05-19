eec62ba70265cc4b503dc30bdceb010fccec06bd
vitekkor <viktor.korotkikh@gmail.com>
2023-05-20 01:23:50 +0300

    #7: change BaseCommand signature

--- compoly-bot/src/main/kotlin/com/vitekkor/compolybot/scenario/command/BaseCommand.kt
+++ compoly-bot/src/main/kotlin/com/vitekkor/compolybot/scenario/command/BaseCommand.kt
@@ -1,8 +1,21 @@
 package com.vitekkor.compolybot.scenario.command
 
+import com.justai.jaicf.api.BotRequest
+import com.justai.jaicf.builder.StateBuilder
+import com.justai.jaicf.builder.createModel
 import com.justai.jaicf.model.scenario.Scenario
+import com.justai.jaicf.model.scenario.ScenarioModel
+import com.justai.jaicf.reactions.Reactions
 
-abstract class BaseCommand: Scenario {
+abstract class BaseCommand : Scenario {
     abstract val name: String
     abstract val description: String
+
+    abstract fun StateBuilder<BotRequest, Reactions>.commandAction()
+
+    override val model: ScenarioModel = createModel {
+        state(name) {
+            commandAction()
+        }
+    }
 }
--- compoly-bot/src/main/kotlin/com/vitekkor/compolybot/scenario/command/PingCommand.kt
+++ compoly-bot/src/main/kotlin/com/vitekkor/compolybot/scenario/command/PingCommand.kt
@@ -1,22 +1,20 @@
 package com.vitekkor.compolybot.scenario.command
 
-import com.justai.jaicf.builder.Scenario
-import com.justai.jaicf.model.scenario.ScenarioModel
-import com.justai.jaicf.model.scenario.getValue
+import com.justai.jaicf.api.BotRequest
+import com.justai.jaicf.builder.StateBuilder
+import com.justai.jaicf.reactions.Reactions
 import org.springframework.stereotype.Component
 
 @Component
 class PingCommand : BaseCommand() {
     override val name: String = "пинг"
     override val description: String = "Pong!"
-    override val model: ScenarioModel by Scenario {
-        state("ping") {
-            activators {
-                regex("/(пинг)|(pong)")
-            }
-            action {
-                reactions.say("Pong!")
-            }
+    override fun StateBuilder<BotRequest, Reactions>.commandAction() {
+        activators {
+            regex("/(пинг)|(pong)")
+        }
+        action {
+            reactions.say("Pong!")
         }
     }
 }
